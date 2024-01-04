ad765df3d9f5975ac98ba787a1cd95602138d1aa
pedro <pluvet01@gmail.com>
2024-01-03 20:39:54 -0500

    improve goroutine

--- app/events/account.go
+++ app/events/account.go
@@ -1,22 +1,17 @@
 package events
 
 import (
-	"sync"
-
 	"github.com/pluvet/go-bank/app/config"
 	"github.com/pluvet/go-bank/app/models"
 )
 
 func CreateAccountEvent(userId uint) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	CreateAccountHandler(&wg, userId)
+	go CreateAccountHandler(userId)
 }
 
-func CreateAccountHandler(wg *sync.WaitGroup, userId uint) {
+func CreateAccountHandler(userId uint) {
 	var account models.Account
 	account.Balance = 0
 	account.UserID = uint64(userId)
 	config.DB.Create(&account)
-	defer wg.Done()
 }
